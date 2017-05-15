f4aafd15e2c6c95d542af825e9d1f2370917025d
elizajasin <eliza_jasin@hotmail.com>
2017-05-16 01:44:30 +0700

    add tagging

--- Main.py
+++ Main.py
@@ -5,4 +5,3 @@ import TrainTest as TnT
 
 data = PreP.readData("E:/Kuliah/semester 8/NLP/Tubes/sa-tagged")
 case_fold = PreP.caseFolding(data)
-print(TnT.tagging(case_fold))
--- TrainTest.py
+++ TrainTest.py
@@ -1,47 +1,38 @@
 __author__ = 'elizajasin'
 
+import re
+import operator
+
 def tagging (data):
-    tags = []
-    words = []
-    # learning
-    for imel in range(len(data)):
-        abstract = str(data[imel]).find('abstract')
-        abstract += 9
-        s = ""
-        r = ""
-        startT = True
-        startW = True
-        open = False
-        close = False
-        word = []
-        tag = []
-        for isi in range(abstract, len(data[imel]), 1):
-            if open:
-                s += data[imel][isi]
-            if close and startW:
-                if data[imel][isi] != '>' or data[imel][isi] != '<':
-                    r += data[imel][isi]
-            if data[imel][isi] == '<':
-                if startW:
-                    startT = True
-                s += data[imel][isi]
-                startW = True
-                open = True
-                close = False
-            if data[imel][isi] == '>':
-                if startW == False:
-                    startT = False
-                close = True
-                open = False
-            if data[imel][isi] == '/':
-                startW = False
-                word.append(r)
-                r = ""
-            if startT == False:
-                startT = True
-                tag.append(s)
-                s = ""
-        print(word[1],tag[1])
-        tags.append(s)
-        words.append(r)
-    return tags
+    #create bag of words for emission probability using unigram model
+    bagOfWords_time = {}
+    bagOfWords_loc = {}
+    bagOfWords_speak = {}
+    for i in range(len(data)):
+        #search the pattern of each class from data using regex
+        match_time = re.search(r'<stime>(.+)</stime>',data[i])
+        match_location = re.search(r'<location>(.+)</location>',data[i])
+        match_speaker = re.search(r'<speaker>(.+)</location>',data[i])
+        #count all words with its tag
+        if match_time != None:
+            words_time = str(match_time.groups()[0]).split()
+            for j in range(len(words_time)):
+                if words_time[j] in bagOfWords_time.keys():
+                    bagOfWords_time[words_time[j]] = bagOfWords_time[words_time[j]]+1
+                else:
+                    bagOfWords_time[words_time[j]] = 1
+        if match_location != None:
+            words_loc = str(match_location.groups()[0]).split()
+            for j in range(len(words_loc)):
+                if words_loc[j] in bagOfWords_loc.keys():
+                    bagOfWords_loc[words_loc[j]] = bagOfWords_loc[words_loc[j]]+1
+                else:
+                    bagOfWords_loc[words_loc[j]] = 1
+        if match_speaker != None:
+            words_speaker = str(match_speaker.groups()[0]).split()
+            for j in range(len(words_speaker)):
+                if words_speaker[j] in bagOfWords_speak.keys():
+                    bagOfWords_speak[words_speaker[j]] = bagOfWords_speak[words_speaker[j]]+1
+                else:
+                    bagOfWords_speak[words_speaker[j]] = 1
+    return bagOfWords_time,bagOfWords_loc,bagOfWords_speak
