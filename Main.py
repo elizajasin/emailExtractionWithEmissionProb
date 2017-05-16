42b484b9bab2f090f67c95cff0ef65ab61d46d32
elizajasin <eliza_jasin@hotmail.com>
2017-05-16 08:47:20 +0700

    add testing and accuracy

--- Main.py
+++ Main.py
@@ -5,3 +5,6 @@ import TrainTest as TnT
 
 data = PreP.readData("E:/Kuliah/semester 8/NLP/Tubes/sa-tagged")
 case_fold = PreP.caseFolding(data)
+ep_time,ep_loc,ep_speak = TnT.train(case_fold,350)
+test_time,tes_loc,tes_speak,true_time,true_loc,true_speak = TnT.tes(case_fold,350,ep_time,ep_loc,ep_speak)
+print(TnT.akurasi(test_time,tes_loc,tes_speak,true_time,true_loc,true_speak))
--- TrainTest.py
+++ TrainTest.py
@@ -12,7 +12,7 @@ def tagging (data):
         #search the pattern of each class from data using regex
         match_time = re.search(r'<stime>(.+)</stime>',data[i])
         match_location = re.search(r'<location>(.+)</location>',data[i])
-        match_speaker = re.search(r'<speaker>(.+)</location>',data[i])
+        match_speaker = re.search(r'<speaker>(.+)</speaker>',data[i])
         #count all words with its tag
         if match_time != None:
             words_time = str(match_time.groups()[0]).split()
@@ -55,8 +55,108 @@ def emission_probability(bagOfWords_time,bagOfWords_loc,bagOfWords_speak):
     return ep_time,ep_loc,ep_speak
 
 def train(data,n):
+    # training process
     data_train = []
     for i in range(n):
-        data_train[i] = data[i]
+        data_train.append(data[i])
     bagOfWords_time,bagOfWords_loc,bagOfWords_speak = tagging(data_train)
     return emission_probability(bagOfWords_time,bagOfWords_loc,bagOfWords_speak)
+
+def tes(data,n,ep_time,ep_loc,ep_speak):
+    # testing process
+    true_time = 0
+    test_time = 0
+    true_loc = 0
+    tes_loc = 0
+    true_speak = 0
+    tes_speak = 0
+    data_test = []
+    for i in range(n,len(data)):
+        data_test.append(data[i])
+    for i in range(len(data_test)):
+        #search the pattern of each class from data using regex
+        match_time = re.search(r'<stime>(.+)</stime>',data[i])
+        match_location = re.search(r'<location>(.+)</location>',data[i])
+        match_speaker = re.search(r'<speaker>(.+)</speaker>',data[i])
+        if match_time != None:
+            test_time += 1
+            time = str(match_time.groups()[0]).split();
+            for i in range(len(time)):
+                prob = {}
+                if time[i] in ep_time.keys():
+                    if "time" in prob.keys():
+                        if prob["time"] < ep_time[time[i]]:
+                            prob["time"] = ep_time[time[i]]
+                    else:
+                        prob["time"] = ep_time[time[i]]
+                if time[i] in ep_loc.keys():
+                    if "loc" in prob.keys():
+                        if prob["loc"] < ep_time[time[i]]:
+                            prob["loc"] = ep_time[time[i]]
+                    else:
+                        prob["loc"] = ep_time[time[i]]
+                if time[i] in ep_speak.keys():
+                    if "speak" in prob.keys():
+                        if prob["speak"] < ep_time[time[i]]:
+                            prob["speak"] = ep_time[time[i]]
+                    else:
+                        prob["speak"] = ep_time[time[i]]
+            if max(prob.values()) == prob["time"]:
+                true_time += 1
+        if match_location != None:
+            tes_loc += 1
+            loc = str(match_location.groups()[0]).split();
+            for i in range(len(loc)):
+                prob = {}
+                if loc[i] in ep_time.keys():
+                    if "time" in prob.keys():
+                        if prob["time"] < ep_time[loc[i]]:
+                            prob["time"] = ep_time[loc[i]]
+                    else:
+                        prob["time"] = ep_time[loc[i]]
+                if loc[i] in ep_loc.keys():
+                    if "loc" in prob.keys():
+                        if prob["loc"] < ep_loc[loc[i]]:
+                            prob["loc"] = ep_loc[loc[i]]
+                    else:
+                        prob["loc"] = ep_loc[loc[i]]
+                if loc[i] in ep_speak.keys():
+                    if "speak" in prob.keys():
+                        if prob["speak"] < ep_loc[loc[i]]:
+                            prob["speak"] = ep_loc[loc[i]]
+                    else:
+                        prob["speak"] = ep_loc[loc[i]]
+            if max(prob.values()) == prob["loc"]:
+                true_loc += 1
+        if match_speaker != None:
+            tes_speak += 1
+            speak = str(match_speaker.groups()[0]).split();
+            for i in range(len(speak)):
+                prob = {}
+                if speak[i] in ep_time.keys():
+                    if "time" in prob.keys():
+                        if prob["time"] < ep_speak[speak[i]]:
+                            prob["time"] = ep_speak[speak[i]]
+                    else:
+                        prob["time"] = ep_speak[speak[i]]
+                if speak[i] in ep_loc.keys():
+                    if "loc" in prob.keys():
+                        if prob["loc"] < ep_speak[speak[i]]:
+                            prob["loc"] = ep_speak[speak[i]]
+                    else:
+                        prob["loc"] = ep_speak[speak[i]]
+                if speak[i] in ep_speak.keys():
+                    if "speak" in prob.keys():
+                        if prob["speak"] < ep_speak[speak[i]]:
+                            prob["speak"] = ep_speak[speak[i]]
+                    else:
+                        prob["speak"] = ep_speak[speak[i]]
+            if max(prob.values()) == prob["speak"]:
+                true_speak += 1
+    return test_time,tes_loc,tes_speak,true_time,true_loc,true_speak
+
+def akurasi (test_time,tes_loc,tes_speak,true_time,true_loc,true_speak):
+    test = test_time+tes_loc+tes_speak
+    true = true_time+true_loc+true_speak
+    accuracy = true/test*100
+    return (str(accuracy)+'%')
